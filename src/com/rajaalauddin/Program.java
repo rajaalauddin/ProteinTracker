61d6225b006aa49276692dfd4ba6e2e3363e555d
Raja Alauddin Raja Abdullah <rajaalauddin.work@gmail.com>
2014-04-20 14:22:03 -0500

    Add basic query

--- src/com/rajaalauddin/Program.java
+++ src/com/rajaalauddin/Program.java
@@ -1,7 +1,9 @@
 package com.rajaalauddin;
 
 import java.util.Date;
+import java.util.List;
 
+import org.hibernate.Query;
 import org.hibernate.Session;
 
 public class Program {
@@ -12,6 +14,8 @@ public class Program {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello world");
+		
+		/*
 		Session session = HibernateUtilities.getSessionFactory().openSession();
 		session.beginTransaction();
 		
@@ -44,7 +48,55 @@ public class Program {
 		
 		session.getTransaction().commit();
 		session.close();
+		*/		
+		PopulateSampleData();
+		
+		Session session = HibernateUtilities.getSessionFactory().openSession();
+		session.beginTransaction();
+		
+		Query query = session.createQuery("from User");
+		List<User> users = query.list();
+		
+		for(User user : users) {
+			System.out.println(user.getName());
+		}
+		
+		session.getTransaction().commit();	
+		session.close();
+		
 		HibernateUtilities.getSessionFactory().close();
+	}
+	
+	private static void PopulateSampleData() {
+		Session session = HibernateUtilities.getSessionFactory().openSession();
+		session.beginTransaction();
+		
+		User joe = CreateUser("Joe", 500, 50, "Good Job", "You made it");
+		session.save(joe);
+		
+		User bob = CreateUser("Bob", 300, 20, "Taco time!");
+		session.save(bob);
+		
+		User amy = CreateUser("Amy", 250, 200, "Yes !!");
+		session.save(amy);
+		session.getTransaction().commit();
+		session.close();
+	}
+	
+	private static User CreateUser(String name, int goal, int total, String ... alerts) {
+		
+		User user = new User();
+		user.setName(name);
+		user.getProteinData().setGoal(goal);
+		user.addHistory(new UserHistory(new Date(), "Set goal to " + goal));
+		user.getProteinData().setTotal(total);
+		user.addHistory(new UserHistory(new Date(), "Set total to " + total));
+		
+		for(String alert : alerts) {
+			user.getGoalAlerts().add(new GoalAlert(alert));
+		}
+		
+		return user;
 	}
 
 }
